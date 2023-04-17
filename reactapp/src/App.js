1a14608b507f3fd517fceab9aefbab402a65c90d
Sarthak Kumar Behera <sarthakbehera09@gmail.com>
2023-04-17 21:18:59 +0530

    Removed router imports from app.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,7 +1,3 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./components/Root/Root";
-import HoamePage from "./components/Customer/HoamePage/HoamePage";
-import Login from "./components/Auth/Login/Login";
 import Signup from "./components/Auth/Signup/Signup";
 
 import "./App.css";
